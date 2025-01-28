a3977a3ac688be7bfa067032a350ade97545d587
Tom <keritamas49@gmail.com>
2025-01-28 13:53:17 +0100

    plusz

--- layout/header.php
+++ layout/header.php
@@ -37,15 +37,17 @@ if (isset($_SESSION["email"])) {
             <ul id="navbar2">    
                 <div class="dropdown-center">
                   <li class="nav-item dropdown dropdown-center">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/></svg></a>
+                    <?php
+                    if ($authenticated) {
+                    ?>
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?= $_SESSION["first_name"] ?>&nbsp;<?= $_SESSION["last_name"] ?></a>
                     <div class="dropdown-menu rounded-0 border border-dark" aria-labelledby="navbarDropdown">
-                      <?php
-                      if ($authenticated) {
-                      ?>
                         <a class="dropdown-item" href="logout.php">Kijelentkezés</a>
                       <?php
                       } else {
                       ?>
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/></svg></a>
+                    <div class="dropdown-menu rounded-0 border border-dark" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="Bejelentkezes.php">Bejelentkezés</a>
                         <a class="dropdown-item" href="Regisztracio.php">Regisztráció</a>
                       <?php } ?>
