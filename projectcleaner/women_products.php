3a8c2ee4fc4ce00ceca91e41ebe33418d8b4e554
TOm <keritamas49@gmail.com>
2025-02-18 23:09:44 +0100

    termekekupdate

--- projectcleaner/women_products.php
+++ projectcleaner/women_products.php
@@ -6,14 +6,14 @@ include "header.php";
 require_once 'fetch_product_datas.php';
 require_once 'generate_product_card.php';
 
-$menProducts = fetchMenProducts();
+$womenProducts = fetchWomenProducts();
 ?>
 
 <section id="ferfitermekcards">
     <div class="container mt-5">
         <div class="row">
             <?php
-            foreach ($menProducts as $product) {
+            foreach ($womenProducts as $product) {
                 echo generateProductCard($product);
             }
             ?>
