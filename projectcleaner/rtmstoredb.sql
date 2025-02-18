741e3a3fe9184be2446058359c3ca0cf6d3d1296
TOm <keritamas49@gmail.com>
2025-02-18 23:47:26 +0100

    db

--- projectcleaner/rtmstoredb.sql
+++ projectcleaner/rtmstoredb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2025. Feb 17. 00:05
+-- Létrehozás ideje: 2025. Feb 18. 23:46
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
@@ -25,8 +25,32 @@ DELIMITER $$
 --
 -- Eljárások
 --
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetMenIngekPolok` ()   BEGIN
+    SELECT * FROM products WHERE gender = 'Férfi' AND category = 'Ingek & Pólók';
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetMenKabatokPuloverek` ()   BEGIN
+    SELECT * FROM products WHERE gender = 'Férfi' AND category = 'Kabátok & Pulóverek';
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetMenNadragokRovidnadragok` ()   BEGIN
+    SELECT * FROM products WHERE gender = 'Férfi' AND category = 'Nadrágok & Rövidnadrágok';
+END$$
+
 CREATE DEFINER=`root`@`localhost` PROCEDURE `GetMenProducts` ()   BEGIN
     SELECT * FROM products WHERE gender = 'Férfi';
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetWomenBluzokPolok` ()   BEGIN
+    SELECT * FROM products WHERE gender = 'Nő' AND category = 'Blúzok & Pólók';
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetWomenKabatokPuloverek` ()   BEGIN
+    SELECT * FROM products WHERE gender = 'Nő' AND category = 'Kabátok & Pulóverek';
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `GetWomenNadragokRovidnadragok` ()   BEGIN
+    SELECT * FROM products WHERE gender = 'Nő' AND category = 'Nadrágok & Rövidnadrágok';
 END$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `GetWomenProducts` ()   BEGIN
@@ -82,7 +106,13 @@ INSERT INTO `products` (`product_id`, `product_name`, `product_price`, `product_
 (8, 'Fekete G-STAR Póló \'Lash\'', 12490, 'uploads/7d0070b3c97845484bbc9695b9770e64.png', 'uploads/d92af3eef8bf940405e23b11076a0687.jpg', 'uploads/bb95b254ee28db00ade37eff102384a2.jpg', 'uploads/4c9b18e6130c05d23e06529d269cbcf2.jpg', 50, 50, 50, 50, 50, 'Férfi', 'Ingek & Pólók', '2025-02-16 22:11:42'),
 (9, 'JACK & JONES Slimfit Chino nadrág', 26990, 'uploads/76ca487808f323449281c45f453d4ab7.jpg', 'uploads/4836a9023b13b3cd8329738a47cb13e4.jpg', 'uploads/a99c2797bf67c3c584650f3b046e9d41.jpg', 'uploads/26c71d4430b99c8fb6787ed8e0dfa0d0.png', 50, 50, 50, 50, 50, 'Férfi', 'Nadrágok & Rövidnadrágok', '2025-02-16 22:15:42'),
 (10, 'Piszkosfehér HOLLISTER Szabványos Nadrág', 16490, 'uploads/256ae4c42ad57a74973f8257b486f10a.jpg', 'uploads/f27b1ec7307b1dfd02fe6b633a62669d.png', 'uploads/7c0e8336c2079ecab3f78c596c2d1e8c.jpg', 'uploads/ce7c6fca730b80db3a55a37974d6dee7.jpg', 50, 50, 50, 50, 50, 'Férfi', 'Nadrágok & Rövidnadrágok', '2025-02-16 22:20:29'),
-(11, 'Fekete JACK & JONES Slim fit Ing \'Cardiff\'', 14490, 'uploads/91ef05ec7e9671c1ade6a9ba054ffb87.jpg', 'uploads/44e48ef13e52fa99dc632a21f570ee7b.jpg', 'uploads/9d3f37d5aabca147462e1ce170d15647.jpg', 'uploads/71b2e543551d64e53fbc2de598873c67.jpg', 50, 50, 50, 50, 50, 'Férfi', 'Ingek & Pólók', '2025-02-17 00:00:58');
+(11, 'Fekete JACK & JONES Slim fit Ing \'Cardiff\'', 14490, 'uploads/91ef05ec7e9671c1ade6a9ba054ffb87.jpg', 'uploads/44e48ef13e52fa99dc632a21f570ee7b.jpg', 'uploads/9d3f37d5aabca147462e1ce170d15647.jpg', 'uploads/71b2e543551d64e53fbc2de598873c67.jpg', 50, 50, 50, 50, 50, 'Férfi', 'Ingek & Pólók', '2025-02-17 00:00:58'),
+(12, 'Fekete VERO MODA Télikabátok \'VMHALSEY\'', 28990, 'uploads/d48150d8a5eaef2f1d08d8ba65fd8e3c.jpg', 'uploads/d2b7682f694425962b74f8740cf67f4b.jpg', 'uploads/c416be9497a39bb1e534b6020b02552d.jpg', 'uploads/0d7d85abab086411483cc07508eae8e0.jpg', 50, 50, 50, 50, 50, 'Nő', 'Kabátok & Pulóverek', '2025-02-18 21:06:16'),
+(13, 'Vegyes Színek ONLY Pulóver \'Atia\'', 13490, 'uploads/3f3951c871331328ea888379e620fad4.jpg', 'uploads/d0ddaadeaae7d269868a8864af056387.jpg', 'uploads/bcd789a09512ecb5738b190c1355c679.jpg', 'uploads/7046c03ca61eaebd7cd1818b5ecd2ac2.jpg', 50, 50, 50, 50, 50, 'Nő', 'Kabátok & Pulóverek', '2025-02-18 21:09:08'),
+(15, 'Krém VILA Blúz \'VIChikka\'', 16490, 'uploads/4d441417167dc072198359b8d3987abd.jpg', 'uploads/b12e1380dc31307096332e3311396661.png', 'uploads/8ecca33dc068f741c9487a860cb0fa47.jpg', 'uploads/54d0d95fe36d3358ec5e617b55cc576d.jpg', 50, 50, 50, 50, 50, 'Nő', 'Blúzok & Pólók', '2025-02-18 21:17:58'),
+(16, 'Fekete JDY Bő szár Nadrág', 10490, 'uploads/30f9e98049a0bf7ca5dcf433688df7f9.jpg', 'uploads/6d0d5bc3af3d37865f7eb5af00ec0399.png', 'uploads/049cd64259b88784201502d35af285a5.jpg', 'uploads/fa28129f8676ffe3ae463a05c5619727.jpg', 50, 50, 50, 50, 50, 'Nő', 'Nadrágok & Rövidnadrágok', '2025-02-18 21:21:15'),
+(17, 'Krém Guido Maria Kretschmer Women Szabványos Nadrág \'Cara\'', 16490, 'uploads/300b05f4a8642bd8afe9274677f0aeb1.jpg', 'uploads/c9170d7945948667fccd4dd96fe40ec7.png', 'uploads/2bc60521e3af7df414667c8f9cd75041.jpg', 'uploads/ad125aeeebe5b79140cdf2e55b639dea.jpg', 50, 50, 50, 50, 50, 'Nő', 'Nadrágok & Rövidnadrágok', '2025-02-18 21:27:53'),
+(18, 'Fehér LEVI\'S ® Póló \'PERFECT\'', 10490, 'uploads/9071270cd69fec4ea0f651f1ff036b37.png', 'uploads/c6da258d740511735f2b7deb9b3b88e2.jpg', 'uploads/219d5548de8334cc8bbd9688bda0e33d.jpg', 'uploads/a6762d780372da82691c06275fecccac.jpg', 50, 50, 50, 50, 50, 'Nő', 'Blúzok & Pólók', '2025-02-18 21:37:46');
 
 --
 -- Indexek a kiírt táblákhoz
@@ -102,7 +132,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT a táblához `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
